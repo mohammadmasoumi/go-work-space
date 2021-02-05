f9c414c6ac296a7e2d5e39d48d31f4b5f8639a2a
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-02-05 23:13:18 +0330

    clean code

--- 001-helloworld/main.go
+++ 001-helloworld/main.go
@@ -6,14 +6,14 @@ import "fmt"
 create go module
  - go mod init PATH_TO_THE_MAIN.GO
  - go mod tidy
- */
+*/
 
 /**
 control flow:
   (1) sequence
-  (2) loop: iterative
+  (2) loop: iterativea
   (3) conditional
- */
+*/
 
 // the entrypoint to your project
 func main() {

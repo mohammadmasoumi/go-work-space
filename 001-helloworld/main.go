5b9edfa4c4a29012e8765aa26dc5de02218b7df1
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-02-06 23:36:25 +0330

    add statements

--- 001-helloworld/main.go
+++ 001-helloworld/main.go
@@ -62,8 +62,15 @@ func main() {
 	// it declare and assign values
 	x := 42
 	fmt.Println("x: ", x)
+	// assign
 	x = 2
 	fmt.Println("x; ", x)
+
+	// statement that is made up of expressions
+	y := x + 100
+	fmt.Println(y)
+
+
 
 }
 

e7f51bd8dbee2fb14bc4bd43d335646617133acc
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-02-07 00:51:00 +0330

    add packaging

--- 001-helloworld/main.go
+++ 001-helloworld/main.go
@@ -31,23 +31,6 @@ func main() {
 	}
 
 	bar()
-
-	/**
-	godoc.org - packages
-	func Println(a ...interface{}) (n int, err error)
-
-	[variadic parameter]
-
-	"... <some types>" is how we signify a variadic parameter
-	"interface{}" is the empty interface
-	so the parameter "... interface{}" means "give me as many arguments of any type as you'd like"
-
-	Println formats using the default formats for its operands and writes to standard output. Spaces are always added between
-	operands and a newline is appended. It returns the number of bytes written and any write error encountered.
-	*/
-	// use the "_" underscore character to throw away returns
-	n, _ := fmt.Println("Hello", 42, true)
-	fmt.Println(n) // number of bytes, it's written
 }
 
 func foo() {

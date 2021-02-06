8e562d816b970119bf997f30a5ccad38b08c1948
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-02-06 23:25:53 +0330

    add comment

--- 001-helloworld/main.go
+++ 001-helloworld/main.go
@@ -45,6 +45,7 @@ func main() {
 	Println formats using the default formats for its operands and writes to standard output. Spaces are always added between
 	operands and a newline is appended. It returns the number of bytes written and any write error encountered.
 	 */
+	// use the "_" underscore character to throw away returns
 	n, _ := fmt.Println("Hello", 42, true)
 	fmt.Println(n) // number of bytes, it's written
 

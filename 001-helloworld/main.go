e54e38c993a15828266016e5b315a041322db8b7
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-02-06 23:16:55 +0330

    add package doc

--- 001-helloworld/main.go
+++ 001-helloworld/main.go
@@ -36,6 +36,8 @@ func main() {
 	godoc.org - packages
 	func Println(a ...interface{}) (n int, err error)
 
+	Println formats using the default formats for its operands and writes to standard output. Spaces are always added between
+	operands and a newline is appended. It returns the number of bytes written and any write error encountered.
 	 */
 	n, _ := fmt.Println("Hello", 42, true)
 	fmt.Println(n)

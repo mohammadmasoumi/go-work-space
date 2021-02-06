04d957d2a2c0a799f6edf15fe2a987ed515670de
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-02-06 23:24:38 +0330

    add comment

--- 001-helloworld/main.go
+++ 001-helloworld/main.go
@@ -38,6 +38,10 @@ func main() {
 
 	[variadic parameter]
 
+	"... <some types>" is how we signify a variadic parameter
+	"interface{}" is the empty interface
+	so the parameter "... interface{}" means "give me as many arguments of any type as you'd like"
+
 	Println formats using the default formats for its operands and writes to standard output. Spaces are always added between
 	operands and a newline is appended. It returns the number of bytes written and any write error encountered.
 	 */

42929a891ca52974e7e98f533e9cab75fbc597cc
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-02-08 00:05:02 +0330

    update code

--- 004-var-keyword/main.go
+++ 004-var-keyword/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
 	// DECLARE a variable and ASSIGN a VALUE at the same time.
-	// := is an operator because is operating on a operand
+	// := is an operator because is operating on these operands
 	x := 42
 	fmt.Println("x is: ", x)
 

0abfc8b670af2d6b867483087cbf991caa2867db
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-02-08 00:33:26 +0330

    add zero value

--- 004-var-keyword/main.go
+++ 004-var-keyword/main.go
@@ -4,7 +4,26 @@ import "fmt"
 
 // it's like global variable to this file
 // the scope of this variable is the current file
+// the package level scope
 var outsideParams = "Hello"
+
+/**
+DECLARE there is a VARIABLE with the IDENTIFIER "z"
+and that the VARIABLE with the IDENTIFIER "z" is of TYPE int
+ASSIGNS the ZERO VALUE of TYPE int to "z"
+*/
+var z int
+
+/**
+Zero values
+Variables declared without an explicit initial value are given their zero value.
+
+The zero value is:
+
+0 for numeric types,
+false for the boolean type, and
+"" (the empty string) for strings.
+*/
 
 func main() {
 
@@ -12,18 +31,35 @@ func main() {
 	// DECLARE a variable and ASSIGN a VALUE (of a certain TYPE)
 	// := is an operator because is operating on these operands
 	x := 42
-	fmt.Println("x is: ", x)
+	fmt.Println("x is:", x)
 
+	// variable
 	var y = 43
-	fmt.Println("y is: ", y)
+	fmt.Println("y is:", y)
+
+	// assign value to the variable z
+	z = 25
+
+	// function call
+	foo()
+	zeroValues()
 
 	/**
 	What is the difference between var and gopher operator?
 		- non-declaration statement outside function body.
-
+		- we can assign vars later, vars will hold their default values
 	*/
 }
 
 func foo() {
-	fmt.Println(outsideParams)
+	fmt.Println("outsideParams:", outsideParams)
+	fmt.Println("z:", z)
+}
+
+func zeroValues() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("i: %v,  f: %v, b: %v, s: %q\n", i, f, b, s)
 }

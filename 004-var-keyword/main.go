e5777f651561c2142d037db975bd20bdfb70a724
MohammadMasoumi <mohammad.masoomy74@gmail.com>
2021-02-08 00:06:39 +0330

    add comment

--- 004-var-keyword/main.go
+++ 004-var-keyword/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
 	// short decoration operator
-	// DECLARE a variable and ASSIGN a VALUE at the same time.
+	// DECLARE a variable and ASSIGN a VALUE (of a certain TYPE)
 	// := is an operator because is operating on these operands
 	x := 42
 	fmt.Println("x is: ", x)
